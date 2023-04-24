1059bb43bc60633bab8685db057343ca49c52f33
Pip <pip.harsveld@hva.nl>
2023-04-24 14:38:16 +0200

    Implement hbs

--- server.js
+++ server.js
@@ -2,20 +2,35 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import http from 'http';
+import handlebars from 'express-handlebars';
 import { Server } from 'socket.io';
-// import cookieParser from 'cookie-parser';
+import { router } from './router/router.js'; 
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4242;
+const __dirname = path.resolve();
 const server = http.createServer(app);
 const io = new Server(server);
 const historySize = 50
 let history = []
 
+// Link the templating engine to the express app
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-app.use(express.static(path.resolve('public')));
+
+app.use('/', router);
+app.use(express.static(__dirname + '/static')); //Css, images en javascript
+app.use('/', express.static(__dirname + '/'));
+
+app.engine('hbs', handlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'index',
+    partialsDir: __dirname + '/views/partials'
+}));
 
 
 io.on('connection', (socket) => {
@@ -33,7 +48,6 @@ io.on('connection', (socket) => {
         io.sockets.emit("chat", data);
     });
 
-
     socket.on('typing', (inputName) => {
         console.log("Aan het typen");
         socket.broadcast.emit("typing", inputName);
@@ -48,3 +62,4 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
