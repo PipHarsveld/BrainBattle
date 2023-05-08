9de781960dcee5c6155f8d97a59e7bafb67ed984
Pip <pip.harsveld@hva.nl>
2023-05-08 11:34:59 +0200

    Get rooms working
    Made a lot of changes, but finally got the rooms working (I think). The roomnumber is now generated client side and send to the server with the emit. I have an empty erray which get filled with the newly made roomnumbers. I added validation and check if the room a user wants to join, is in the array.

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const __dirname = path.resolve();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Add a new array to keep track of active rooms
+const activeRooms = [];
+
 // Link the templating engine to the express app
 app.set('view engine', 'hbs');
 app.set('views', 'views');
@@ -32,52 +35,77 @@ app.get('/', (req, res) => {
     res.render('main', { layout: 'index' });
 })
 
-app.get("/room/:roomNumber", (req, res) => {
+app.get("/room/:roomNumber", (req, res, next) => {
     const { roomNumber } = req.params;
+    console.log(roomNumber)
+    console.log(io.sockets.adapter.rooms)
 
-    // Render the room page with the room number passed as a parameter
+    // Check if the room is active
+    if (!activeRooms.includes(roomNumber)) {
+        console.log("Heb even gekeken, maar dit roomnummer staat niet in de array")
+        // If the room does not exist, render an error page or redirect the user
+        // return res.render("error", { layout: "index", message: "Room not found" });
+    }
+
+    // If the room exists, render the room page with the room number passed as a parameter
     res.render("room", { layout: "index", roomNumber });
 });
+
+
+// app.get("/quiz", async (req, res) => {
+//     try {
+//       const response = await fetch("https://the-trivia-api.com/questions?limit=10");
+//       console.log('response: ' + response)
+//       const data = await response.text(); // convert the response to a string
+//       console.log('data: ' + data)
+//       const questions = JSON.parse(data); // parse the string into JSON
+//         console.log('questions: ' + questions)
+
+
+//       res.json({ question: questions[0].question });
+//     } catch (err) {
+//       console.error(err.message);
+//       res.status(500).send("An error occurred while retrieving the questions.");
+//     }
+// });
+
 
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    socket.on("createRoom", () => {
-        const roomNumber = generateRoomNumber();
+    socket.on("createRoom", (room) => {
+        console.log(socket.rooms);
+        const roomNumber = room;
         socket.join(roomNumber);
+        console.log(socket.rooms);
         console.log(`Room ${roomNumber} created`);
-
-        // Add the room to the adapter rooms object
-        // io.sockets.adapter.rooms.set(roomNumber, new Set());
-
-
-        // const roomNumbers = Array.from(io.sockets.adapter.rooms.keys());
-        // console.log(roomNumbers);
-
+    
+        // Add the room to the activeRooms array
+        activeRooms.push(roomNumber);
+        console.log('activeRooms updated', activeRooms);
+    
+        // Emit the roomCreated event only to the socket that triggered the createRoom event
         socket.emit("roomCreated", roomNumber);
     });
 
     socket.on("joinRoom", ({ username, roomNumber }) => {
-        console.log(io.sockets.adapter.rooms);
-        // if (io.sockets.adapter.rooms.has(roomNumber)) {
+        // Check if the room is active
+        const socketRooms = socket.rooms;
+        console.log('joinRoom', socketRooms);
+        if (activeRooms.includes(roomNumber)) {
             console.log(`User ${username} joined room ${roomNumber}`);
             socket.join(`${roomNumber}`);
             socket.emit("roomJoined", { roomNumber, username });
-        // } else {
-            // console.log(`Room ${roomNumber} does not exist`);
-        // }
+        } else {
+            console.log(`Room ${roomNumber} does not exist`);
+        }
     });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
-
-
-const generateRoomNumber = () => {
-    return Math.floor(Math.random() * 90000) + 10000;
-};
 
 
 server.listen(port, () => {
--- static/js/script.js
+++ static/js/script.js
@@ -1,37 +1,46 @@
 let socket = io();
-const createRoomBtn = document.getElementById("create-room-btn");
+const createRoomBtn = document.querySelector("#create-room-btn");
 const joinRoomBtn = document.querySelector("#join-room-btn");
 const roomNumberInput = document.querySelector("#room-number");
 const usernameInput = document.querySelector("#username");
 
-createRoomBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+function generateRoomNumber () {
+    let roomNumber = Math.floor(Math.random() * 9000) + 1000;
+    return roomNumber;
+};
 
-  // Emit the createRoom event to the server
-  socket.emit("createRoom");
+
+createRoomBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const randomnumber = generateRoomNumber();
+    const roomnumber = randomnumber.toString();
+
+    // Emit the createRoom event to the server
+    socket.emit("createRoom", roomnumber);
 });
 
 socket.on("roomCreated", (roomNumber) => {
-  console.log(`Room ${roomNumber} created`);
-  window.location.href = `/room/${roomNumber}`;
+    console.log(`Room ${roomNumber} created`);
+    window.location.href = `/room/${roomNumber}`;
 });
 
 joinRoomBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const username = usernameInput.value;
-  const roomNumber = roomNumberInput.value;
+    e.preventDefault();
+    const username = usernameInput.value;
+    const roomNumber = roomNumberInput.value;
 
-  // Emit the joinRoom event to the server
-  socket.emit("joinRoom", {
-    username,
-    roomNumber,
-  });
+    // Emit the joinRoom event to the server
+    socket.emit("joinRoom", {
+        username,
+        roomNumber,
+    });
 
-  // Listen for the roomJoined event from the server
-  socket.on("roomJoined", ({ roomNumber, username }) => {
-    console.log(`${username} joined room ${roomNumber}`);
-    console.log(roomNumber)
-    window.location.href = `/room/${roomNumber}`;
-  });
+    // Listen for the roomJoined event from the server
+    socket.on("roomJoined", ({ roomNumber, username }) => {
+        console.log(`${username} joined room ${roomNumber}`);
+        console.log(roomNumber)
+        window.location.href = `/room/${roomNumber}`;
+    });
 });
 
